d3619b456cd58bf2681ff81d2b3a2dc19b7835d7
Brandon <brandoncasa8@gmail.com>
2023-02-04 12:37:46 -0500

    can now emulate db and server and friends

--- server/src/index.js
+++ server/src/index.js
@@ -1,43 +1,125 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import { MongoMemoryServer } from 'mongodb-memory-server';
+import path from 'path';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import multer from 'multer';
+import helmet from 'helmet';
+import morgan from 'morgan';
+import { fileURLToPath } from 'url';
+import winston from 'winston';
+import passport from 'passport';
+import session from 'express-session';
+import LocalStrategy from 'passport-local';
+import cookieParser from 'cookie-parser';
+import authRoutes from './routes/auth.js';
+import userRoutes from './routes/user.js';
+import { register } from './controllers/auth.js';
+import { User } from './models/User.js';
 
 dotenv.config();
 
-let mongod = null;
+// Configurations
+const fileName = fileURLToPath(import.meta.url);
+const dirName = path.dirname(fileName);
 
-async function startMongoDevServer() {
-  // This will create an new instance of "MongoMemoryServer" and automatically start it
-  mongod = new MongoMemoryServer({
-    instance: {
-      port: 27017,
-      dbName: 'rebound',
-      dbPath: './mongod/',
-    },
-    binary: {
-      version: '6.0.4',
-    },
-  });
+const mongodbDev = await MongoMemoryServer.create({
+  instance: {
+    port: 27017,
+    dbName: 'rebound',
+    dbPath: './mongod/',
+    auth: false,
+  },
+  binary: {
+    version: '6.0.4',
+  },
+});
 
-  await mongod.start();
+const app = express();
 
-  return mongod;
-}
+const logger = winston.createLogger({
+  level: 'error',
+  transports: [new winston.transports.Console()],
+});
+
+app.use((err, req, res, next) => {
+  logger.error(err.stack);
+  next(err);
+});
+
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: 'cross-origin' }));
+app.use(morgan('common'));
+app.use(express.json({ limit: '30mb', extended: true }));
+app.use(express.urlencoded({ limit: '30mb', extended: true }));
+app.use(cors());
+app.use(cookieParser(process.env.SECRET));
+app.use(
+  session({
+    resave: true,
+    saveUninitialized: true,
+    secret: process.env.SECRET,
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
+// File Storage
+const storage = multer.diskStorage({
+  destination(req, file, cb) {
+    cb(null, '  storage/');
+  },
+  filename(req, file, cb) {
+    cb(null, file.originalname);
+  },
+});
+const upload = multer({ storage });
+
+passport.use(new LocalStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+// Routes
+app.post('/api/auth/register', upload.single('picture'), register); // Eventually combine all auth routes with a file checking middleware
+app.use('/api/auth', authRoutes);
+app.use('/api/user', userRoutes);
 
 async function initDev() {
   if (process.env.NODE_ENV === 'development') {
-    mongod = await startMongoDevServer();
-    console.log('URI: ', mongod.getUri());
+    await mongodbDev.stop();
+    await mongodbDev.start(true);
   }
 }
 
 async function startServer() {
-  const app = express();
-
   await initDev();
 
-  app.listen(3000, () => {
-    console.log('Server started on port 3000');
+  // Mongoose Setup
+  const PORT = process.env.PORT || 6001;
+  let connectString = 'mongodb://localhost:27017/';
+  let extraParams = {};
+  if (process.env.NODE_ENV === 'development') {
+    connectString = mongodbDev.getUri();
+    console.log('Connection String:', connectString);
+  } else {
+    extraParams = {
+      user: process.env.DB_USER,
+      pass: process.env.DB_PASS,
+      authSource: 'admin',
+    };
+  }
+  await mongoose.connect(`${connectString}rebound?w=majority`, {
+    retryWrites: true,
+    retryReads: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    ...extraParams,
+  });
+  app.listen(PORT, () => {
+    console.log(`Server running on port: ${PORT}`);
   });
 }
 
