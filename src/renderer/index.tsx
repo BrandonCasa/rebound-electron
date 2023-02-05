2ab0f83a5003d927fc9eedc819b37bc050d60102
Brandon <brandoncasa8@gmail.com>
2023-02-05 10:12:51 -0500

    alright it works

--- src/renderer/index.tsx
+++ src/renderer/index.tsx
@@ -5,11 +5,9 @@ const container = document.getElementById('root')!;
 const root = createRoot(container);
 root.render(<App />);
 
-if (window.electron && window.electron.ipcRenderer) {
-  // calling IPC exposed from preload script
-  window.electron.ipcRenderer.once('ipc-example', (arg) => {
-    // eslint-disable-next-line no-console
-    console.log(arg);
-  });
-  window.electron.ipcRenderer.sendMessage('ipc-example', ['ping']);
-}
+// calling IPC exposed from preload script
+window.electron.ipcRenderer.once('ipc-example', (arg) => {
+  // eslint-disable-next-line no-console
+  console.log(arg);
+});
+window.electron.ipcRenderer.sendMessage('ipc-example', ['ping']);
